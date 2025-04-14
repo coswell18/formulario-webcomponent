d3d72492e59b10e5cd55cbde5df9b7d0ee6c87e0
coswell18 <coswell18@hotmail.com>
2025-04-14 05:59:46 -0500

    Feat: Creación de componentes transversales

---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar() {
+
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg barra-superior-govco"
+        aria-label="Barra superior"
+      >
+        <a
+          href="https://www.gov.co/"
+          target="_blank"
+          aria-label="Portal del Estado Colombiano - GOV.CO"
+        ></a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
